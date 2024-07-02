fa785d34137a01cc78cade2d6e59ce935503ed13
JJvillegas06 <juanjose1967@live.com>
2024-07-01 21:39:13 -0500

    slider fixed

--- main.js
+++ main.js
@@ -27,22 +27,7 @@ function moveToLeft (){
     console.log(slider)
     console.log(mostrar)
     console.log(img)
-    /*slider.style.transform = `translate(-${translate}%)` 
-
-    translate = translate * 2
-    if(translate > 400){
-        translate = 100
-    }
-    if(counter >2){
-        slider.style.transform = `translate(+200%)`
-    }
-    
-
-    
-    console.log(translate)
-    console.log(counter)
-    console.log(slider)*/
-    
+   
 }
 
 btnRight.addEventListener('click',moveToLeft,)
@@ -53,22 +38,8 @@ function moveToRigth(){
         counter = 2
     }
     mostrar = img[counter]  
-
+    
     slider.innerHTML = mostrar.innerHTML
-    
-
-    /*
-    if(slider = img[2]){
-        slider.style.transform = "translate(-100%)" 
-    }
-    if(slider = img[1]){
-        slider.style.transform = "translate(-100%)" 
-    }
-    
-    
-    if(translate2 > 400){
-        translate2 = 100
-    }*/
     console.log(mostrar)
     console.log(counter)
     console.log(slider)
@@ -78,32 +49,7 @@ btnLeft.addEventListener('click',moveToRigth)
 
 function showProject (){
     location.href = img[counter]
+    
 }
 
 slider.addEventListener('click', showProject)
-
-
-
-/*counter++;
-    img = img[counter]
-
-    if(counter> img.length -1 ){
-        counter = 0
-    }else{
-        slider.style.transform = "translate(-100%)"
-
-    }
-    console.log(counter)
-    console.log(img)*/
-
-
-    /*counter = counter -1;
-    img.length = [counter]
-    if(counter < img.length - 3){
-         counter = 2
-    }else{
-        slider.style.transform = "translate(+100%)"
-    }
-    
-    console.log(img)
-    console.log(counter)*/
